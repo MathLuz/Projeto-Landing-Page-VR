0393d87fc4add2e9b18f70f937c0a8742d91a678
matheus.fernandes <matheus.fernandes@lbv.org.br>
2024-05-28 17:48:44 -0300

    Adicinando efeito de transição nas imagens

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const boxTrocaImg = document.querySelectorAll('.boxTrocaImg');
 const fotoTroca = document.querySelectorAll('.fotoTroca');
-
+const fotoHover = ['img/fotos/bike-1.jpg',
+    'img/fotos/bike-4.jpg'];
 var imgSrc = [];
 fotoTroca.forEach(element => {
     imgSrc.push(element.getAttribute('src'))
 });
 
-const fotoHover = ['img/fotos/bike-1.jpg',
-                    'img/fotos/bike-4.jpg'];
-
-boxTrocaImg.forEach((elemento,i)=> {
-    elemento.addEventListener('mouseover',function(){
-        fotoTroca[i].setAttribute('src', fotoHover[i]);
-
+boxTrocaImg.forEach((elemento, i) => {
+    elemento.addEventListener('mouseover', function () {
+        fotoTroca[i].classList.add('hidden');
+        setTimeout(function(){
+            fotoTroca[i].setAttribute('src', fotoHover[i]);
+            fotoTroca[i].classList.remove('hidden');
+        },
+            100
+        )
     });
-    elemento.addEventListener('mouseout', function() {
-        fotoTroca[i].setAttribute('src', imgSrc[i]);
+    elemento.addEventListener('mouseout', function () {
+        fotoTroca[i].classList.add('hidden');
+        setTimeout( function(){
+            fotoTroca[i].setAttribute('src', imgSrc[i]);
+            fotoTroca[i].classList.remove('hidden');
+        },
+            100
+        )
     });
 });
