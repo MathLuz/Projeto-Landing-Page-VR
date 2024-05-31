d690a27730dabb45b6f3ee9ade98ff6f9711c119
matheus.fernandes <matheus.fernandes@lbv.org.br>
2024-05-31 09:01:57 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const boxTrocaImg = document.querySelectorAll('.boxTrocaImg');
 const fotoTroca = document.querySelectorAll('.fotoTroca');
 const fotoHover = ['img/fotos/bike-1.jpg',
-    'img/fotos/bike-4.jpg'];
+                'img/fotos/bike-4.jpg'];
 var imgSrc = [];
 fotoTroca.forEach(element => {
     imgSrc.push(element.getAttribute('src'))
 });
+const transition = 10;
+document.querySelector('.fotoTroca').style.transition = transition*2+'ms';
 
 boxTrocaImg.forEach((elemento, i) => {
     elemento.addEventListener('mouseover', function () {
@@ -14,7 +16,7 @@ boxTrocaImg.forEach((elemento, i) => {
             fotoTroca[i].setAttribute('src', fotoHover[i]);
             fotoTroca[i].classList.remove('hidden');
         },
-            100
+            transition
         )
     });
     elemento.addEventListener('mouseout', function () {
@@ -23,7 +25,7 @@ boxTrocaImg.forEach((elemento, i) => {
             fotoTroca[i].setAttribute('src', imgSrc[i]);
             fotoTroca[i].classList.remove('hidden');
         },
-            100
+            transition
         )
     });
 });
