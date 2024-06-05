66ce773a7442c76c795afdf8c34780ed309ef584
matheus.fernandes <matheus.fernandes@lbv.org.br>
2024-06-05 10:57:32 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const boxTrocaImg = document.querySelectorAll('.boxTrocaImg');
 const fotoTroca = document.querySelectorAll('.fotoTroca');
 const fotoHover = ['img/fotos/bike-1.jpg',
-                'img/fotos/bike-4.jpg'];
+    'img/fotos/bike-4.jpg'];
 var imgSrc = [];
 fotoTroca.forEach(elemento => {
     imgSrc.push(elemento.getAttribute('src'))
 });
 const transition = 50;
-document.querySelectorAll('.fotoTroca').forEach(elemento =>{
-    elemento.style.transition = transition*2+'ms';
+document.querySelectorAll('.fotoTroca').forEach(elemento => {
+    elemento.style.transition = transition * 2 + 'ms';
 })
 
 boxTrocaImg.forEach((elemento, i) => {
+    // MOUSE OVER
     elemento.addEventListener('mouseover', function () {
         fotoTroca[i].classList.add('hidden');
-        setTimeout(function(){
+        setTimeout(function () {
             fotoTroca[i].setAttribute('src', fotoHover[i]);
             fotoTroca[i].classList.remove('hidden');
         },
             transition
         )
     });
+    // MOUSE OUT
     elemento.addEventListener('mouseout', function () {
         fotoTroca[i].classList.add('hidden');
-        setTimeout( function(){
+        setTimeout(function () {
             fotoTroca[i].setAttribute('src', imgSrc[i]);
             fotoTroca[i].classList.remove('hidden');
         },
